27e77b6e9884f2ef2a3905b2128d701206d0b413
v'en'so'm <1281800446@qq.com>
2023-09-08 18:20:14 +0800

    初版功能实现

--- app/controller/main_win_ctr.py
+++ app/controller/main_win_ctr.py
@@ -24,7 +24,6 @@ class MainWinController(QObject):
         self.values = []
         self.current_image = None
         self.vedio_actted.connect(lambda image:self.vedio_show(image), Qt.QueuedConnection)
-        self.show_value.connect(self.enable_ui)
 
     # 初始化ui
     def init_ui(self):
@@ -33,13 +32,12 @@ class MainWinController(QObject):
         self.mw.close_cam.clicked.connect(self.close_camera)
         self.mw.refresh_list.clicked.connect(self.refresh_combox)
         self.mw.auto_star.clicked.connect(self.auto_detect)
-        self.mw.data_output.clicked.connect(self.output_deta)
         self.mw.time_set.textChanged.connect(self.set_time)
     def set_time(self):
         self.time = int(self.mw.time_set.text())
-
     def vedio_show(self,image):
         self.mw.photo_show.rawpix.set_pixmap_with_cvimg(image)
+
     # 打开相机
     def open_camera(self):
         if len(self.mw.cam_combox.currentText()) == 0:
@@ -56,15 +54,13 @@ class MainWinController(QObject):
     def close_camera(self):
         self.cam.close_camera()
         self.mw.photo_show.clear()
-        pass
 
     def refresh_combox(self):
         self.cam.get_camera_information()
         self.mw.cam_combox.clear()
         self.mw.cam_combox.addItems(self.cam.camera_list)
 
-    # 将亮度写入excel表中
-    def write_in_value(self):
+    def auto_detect(self):
         workbook = openpyxl.Workbook()
         worksheet = workbook.sheetnames  # 取第一张表
         worktable = workbook[worksheet[0]]  # 获取第一列
@@ -76,24 +72,8 @@ class MainWinController(QObject):
             rows = rows + 1
         workbook.save('../output/luminance_avg.xlsx')  # 将新数据追加进excel表中
         workbook.close()
-        self.show_value.emit()
+        self.mw.blur_value.setText(str(np.around(np.mean(self.values),2)))
 
-    def enable_ui(self):
-        self.mw.blur_value.setText(str(np.around(np.mean(self.values), 2)))
-
-
-    def auto_detect(self):
-        if self.time == 0:
-            return
-        a_thread = Thread(target=self.write_in_value)
-        a_thread.start()
-
-    def output_deta(self):
-        work_book = openpyxl.Workbook()
-        sheet = work_book.active
-        sheet.title = 'SOMETING'
-        work_book.save('../output/luminance_avg.xls')
-        pass
 
     def work(self, data_buf, size_info):
         # ch:亮度检测 | en:Detect luminance
@@ -109,6 +89,5 @@ class MainWinController(QObject):
         image = image.reshape(size_info.nHeight, size_info.nWidth)  # 根据自己分辨率进行转化
         image, lumi_avg = detect_circle.detect(image)  # 亮度计算
         self.vedio_actted.emit(image)
-
 
 
